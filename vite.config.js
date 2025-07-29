0e60f24348e3bd05ff1a9cce2ba655f43e0e1a0a
infinity-blockchain-team <team@infinityblockchainsolutions.com>
2025-07-29 14:01:26 +0500

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  base: 'e-plantShopping', // Replace with your GitHub repo name
-});
